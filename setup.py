8c99369e5cba5d01730019879cdd678e2772d287
Damini.Gupta <damini.gupta@servicestream.com.au>
2026-02-04 14:31:43 +1100

    adding setup and requirements.txt

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
-python-dotenv
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()-> List[str]:
+    """
+    This function will return list of requirements
+
+    :return:
+    """
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            lines= file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement != "-e .":
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("my requirement.txt file not found" )
+
+    return  requirement_lst
+setup(
+    name="NetworkSecurity",
+    version='0.0.1',
+    author='Damini Gupta',
+    author_email="damini.gupta1@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
+
+
+
