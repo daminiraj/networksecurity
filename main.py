76f19e6a861930e4909455e932688def5cd97144
Damini.Gupta <damini.gupta@servicestream.com.au>
2026-02-10 12:28:43 +1100

    adding data ingestion component

---
+++ main.py
@@ -0,0 +1,22 @@
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.logging.logger import logging
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
+import sys
+
+
+if __name__ == '__main__':
+    try:
+        logging.info("Initiate data ingestion")
+        trainingpipelineconfig= TrainingPipelineConfig()
+        dataingestionconfig= DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion = DataIngestion(data_ingestion_config=dataingestionconfig)
+        logging.info("initiate data ingestion")
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        print(dataingestionartifact)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+
+
