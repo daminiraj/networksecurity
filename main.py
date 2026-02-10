bad89c2e6f04a44c680f6d14590ac2a77c5c8d63
Damini.Gupta <damini.gupta@servicestream.com.au>
2026-02-10 14:47:05 +1100

    adding data validation component

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
 import sys
 
 
@@ -15,6 +17,11 @@ if __name__ == '__main__':
         logging.info("initiate data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         print(dataingestionartifact)
+        logging.info("data initiation completed")
+        data_validation_config= DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        data_validation.initiate_data_validation()
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
 
