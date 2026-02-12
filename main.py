638e067441d32febad7aa66711720b4c43fe45f3
Damini.Gupta <damini.gupta@servicestream.com.au>
2026-02-12 14:28:25 +1100

    adding data transformation component

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.entity.config_entity import DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 import sys
 
 
@@ -20,7 +22,12 @@ if __name__ == '__main__':
         logging.info("data initiation completed")
         data_validation_config= DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
-        data_validation.initiate_data_validation()
+        data_validation_artifact=data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
