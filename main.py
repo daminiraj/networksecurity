fa5bf7bfbb28a59713737e0bfa8c6890b48a64a8
Damini.Gupta <damini.gupta@servicestream.com.au>
2026-02-12 16:00:37 +1100

    adding model training component

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.entity.config_entity import DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.entity.config_entity import ModelTrainerConfig
+from networksecurity.components.model_trainer import ModelTrainer
+
+
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 import sys
@@ -28,6 +32,13 @@ if __name__ == '__main__':
         data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
+        logging.info("Model Training sstared")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,
+                                     data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -5,6 +5,8 @@ import os,sys
 import numpy as np
 import pickle
 # import dill
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def read_yaml_file(file_path:str) -> dict:
     try:
@@ -33,6 +35,12 @@ def save_numpy_array_data(file_path,array:np.array):
             np.save(file_obj,array)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+def load_numpy_array_data(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
 
 def save_obj(file_path,obj):
     try:
@@ -43,3 +51,33 @@ def save_obj(file_path,obj):
         logging.info("Exited save object method of mainutils class")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+def load_obj(file_path):
+    try:
+        logging.info("Load saved object method of mainutils class")
+        if not os.path.exists(file_path):
+            raise Exception (f"The file {file_path} doesnot exist")
+
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+        logging.info("Exited save object method of mainutils class")
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            #model.fit(X_train, y_train)  # Train model
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
